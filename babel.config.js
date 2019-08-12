7b651d7015a40d9ec067dcacf191e6b5b86771c1
yemiancheng <ymc.github@gmail.com>
2019-08-12 11:33:31 +0800

    buid(babel): updating bebel to 7.x

---
+++ babel.config.js
@@ -0,0 +1,36 @@
+﻿module.exports = function (api) {
+	api.cache(true);
+	const presets = [
+	  [
+		'@babel/preset-env',
+		{
+		  targets: {
+			browsers: ['> 1%', 'last 2 versions', 'not ie <= 8']
+		  },
+		  modules: false,
+		  debug: false,
+		  include: [],
+		  exclude: [],
+		  useBuiltIns: false,
+		  forceAllTransforms: false,
+		  ignoreBrowserslistConfig: false,
+		  shippedProposals: false
+		}
+	  ]
+	];
+	const plugins = [
+	  ['@babel/plugin-transform-runtime', {
+		absoluteRuntime: false,
+		corejs: false,
+		helpers: true,
+		regenerator: true,
+		useESModules: false
+	  }
+	  ]
+	];
+  
+	return {
+	  presets,
+	  plugins
+	};
+};
