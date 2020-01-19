0742b293d69e7b96da15a73a91d30b1259a15d11
yemiancheng <ymc.github@gmail.com>
2020-01-20 07:45:18 +0800

    build:update demo code

--- build/webpack.demo-dev.config.js
+++ build/webpack.demo-dev.config.js
@@ -1,5 +1,8 @@
+//include some lib
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path');
 
+//include some data
 const dirConfig = require('../config/dir.construtor');
 const externals = require('./webpack.externals.config');
 const resolve = require('./webpack.resolve.config');
@@ -22,5 +25,37 @@ module.exports = {
       }
     ]
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      //use the index.html file in project dir as his template
+      template: path.resolve(dirConfig.demo, 'src/index.html'),
+      inject: true,
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeAttributeQuotes: true
+        // more options:
+        // https://github.com/kangax/html-minifier#options-quick-reference
+      },
+      // necessary to consistently work with multiple chunks via CommonsChunkPlugin
+      chunksSortMode: 'dependency'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'index02.html',
+      //use the index.html file in project dir as his template
+      template: path.resolve(dirConfig.demo, 'src/index02.html'),
+      inject: false,
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeAttributeQuotes: true
+        // more options:
+        // https://github.com/kangax/html-minifier#options-quick-reference
+      },
+      // necessary to consistently work with multiple chunks via CommonsChunkPlugin
+      chunksSortMode: 'dependency'
+    })
+  ],
   resolve
 };
